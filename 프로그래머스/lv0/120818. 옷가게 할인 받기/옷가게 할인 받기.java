3115f9355bec09ed92bcfbd4b1d787c24950e971
wooni <77907190+wooni89@users.noreply.github.com>
2023-06-11 13:52:52 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.02 ms, Memory: 73.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int price) {
+        int answer = 0;
+        if (500000 <= price) {
+            answer = (int)(price * 0.8);
+        } else if (300000 <= price) {
+            answer = (int)(price * 0.9);
+        } else if (100000 <= price) {
+            answer = (int)(price * 0.95);
+        } else {
+            answer = price;
+        }
+        return answer;
+    }
+}
