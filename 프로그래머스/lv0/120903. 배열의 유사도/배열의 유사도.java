ee03bfce689f01f62cb769a84917fa7dd0a509e8
wooni <77907190+wooni89@users.noreply.github.com>
2023-06-11 14:33:49 +0900

    [level 0] Title: 배열의 유사도, Time: 0.02 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.java
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.java
@@ -1,13 +1,13 @@
 class Solution {
     public int solution(String[] s1, String[] s2) {
         int answer = 0;
-        for (int i = 0; i < s1.length; i++) {
-            for(int j = 0; j < s2.length; j++) {
-                if (s1[i].equals(s2[j])) {
+        for (String str1 : s1) {
+            for  (String str2 : s2) {
+                if(str1.equals(str2)) {
                     answer++;
                 }
             }
         }
-        return answer++;
+        return answer;
     }
 }
