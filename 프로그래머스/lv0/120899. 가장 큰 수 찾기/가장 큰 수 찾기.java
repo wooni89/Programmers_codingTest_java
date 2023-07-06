a5cc4fd2ec7fc4f5c19267d6e89e2ca44468cd39
wooni <77907190+wooni89@users.noreply.github.com>
2023-07-07 01:57:15 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.01 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] solution(int[] array) {
+        int[] answer = new int[2];
+        int max = 0;
+        int count = 0;
+        for (int i = 0 ; i < array.length ;i++) {
+            if (max < array[i]) {
+                max = array[i];
+                answer[0] = max;
+                answer[1] = i;
+            } 
+        }
+        return answer;
+    }
+}
