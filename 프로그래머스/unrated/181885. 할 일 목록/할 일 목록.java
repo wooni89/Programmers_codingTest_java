e169bb30528c5e68fd49ce134463b010baed1e80
wooni <77907190+wooni89@users.noreply.github.com>
2023-08-06 22:13:49 +0900

    [unrated] Title: 할 일 목록, Time: 0.02 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,14 @@
+import java.util.List;
+import java.util.ArrayList;
+
+class Solution {
+    public List solution(String[] todo_list, boolean[] finished) {
+        List<String> answer = new ArrayList<>();
+        for (int i = 0; i < todo_list.length; i++) {
+            if (finished[i] == false) {
+                answer.add(todo_list[i]);
+            }
+        }
+        return answer;
+    }
+}
