74f0834265a32392cd33cb1c9bf8e520e4a16a78
wooni <77907190+wooni89@users.noreply.github.com>
2023-08-22 19:24:59 +0900

    [unrated] Title: 배열 만들기 3, Time: 4.11 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/unrated/181895. 배열 만들기 3/배열 만들기 3.java
+++ 프로그래머스/unrated/181895. 배열 만들기 3/배열 만들기 3.java
@@ -6,14 +6,11 @@ class Solution {
 
         List<Integer> list = new ArrayList<>();
         
-        for(int i = intervals[0][0]; i <= intervals[0][1] && i <= arr.length; i++) {     
-            list.add(arr[i]);
-        }
+        for (int[] intval : intervals)
+            for(int i = intval[0]; i <= intval[1]; i++) {     
+                list.add(arr[i]);
+            }
 
-        for(int i = intervals[1][0]; i <= intervals[1][1] && i <= arr.length; i++) {
-            list.add(arr[i]);
-
-        }
         return list.stream().mapToInt(x -> x).toArray();
     }
 }
