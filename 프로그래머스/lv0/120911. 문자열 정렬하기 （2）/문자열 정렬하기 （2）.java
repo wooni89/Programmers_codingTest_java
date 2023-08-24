5d2b93b23ee67a797f20fd6f5f92b31b2cbcf9cd
wooni <77907190+wooni89@users.noreply.github.com>
2023-08-24 23:21:30 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.42 ms, Memory: 71.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String my_string) {
+        
+        char[] arr = my_string.toLowerCase().toCharArray();
+        
+        Arrays.sort(arr);
+        
+        String answer = new String(arr);
+
+        return answer;
+    }
+}
