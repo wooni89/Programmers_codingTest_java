49bfdea4ef61c121529fecc36bbe75eae3ea137d
wooni <77907190+wooni89@users.noreply.github.com>
2023-09-14 23:49:51 +0900

    [level 0] Title: 안전지대, Time: 0.04 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120866. 안전지대/안전지대.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[][] board) {
+        int answer = 0;
+        int boomRange = 1;
+        int[][] copyBoard = new int[board.length][board[0].length];
+        
+        int tempIndex = 0;
+        for (int[] rows : board) {
+            copyBoard[tempIndex] = Arrays.copyOf(rows, board[tempIndex].length);
+            tempIndex++;
+        }
+        
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                int val = board[i][j];
+                if(val == 0) continue;
+                
+                makeBoomArea(i, j, boomRange, copyBoard);
+            }
+        }
+        
+        for (int[] ints : copyBoard) {
+            for (int anInt : ints) {
+                if (anInt == 0) {
+                    answer ++;
+                }
+            }
+        }
+        return answer;
+    }
+    
+    private void makeBoomArea(int row, int col, int boomRange, int[][] board) {
+        for (int r = row - boomRange; r <= row + boomRange; r++) {
+            if (r < 0 || r >= board.length) continue;
+            
+            for (int c = col - boomRange; c <= col + boomRange; c++) {
+                if (c < 0 || c >= board[0].length) continue;
+                
+                board[r][c] = 1;
+            }
+            
+        }
+    }
+}
