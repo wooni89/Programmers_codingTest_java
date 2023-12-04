799c8bdb8b78156cd4243419de62c88083cf7e58
wooni <77907190+wooni89@users.noreply.github.com>
2023-12-04 17:59:47 +0900

    [level unrated] Title: 다음에 올 숫자, Time: 0.02 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120924. 다음에 올 숫자/다음에 올 숫자.java
+++ 프로그래머스/unrated/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -1,20 +1,16 @@
 class Solution {
     public int solution(int[] common) {
         int answer = 0;
-        int sum = common[1] - common[0];
-        int mul;
-        if (common[0] != 0) {
-           mul = common[1] / common[0];
+
+        int x = common[1] - common[0];
+        int y = common[2] - common[1];
+
+        if (x == y) {
+            answer = common[common.length - 1] + y;
         } else {
-            mul = 1;
+            answer = common[common.length - 1] * common[2] / common[1];
         }
-        
-        if (common[2] - common[1] == sum) {
-            answer = common[(common.length - 1)] + sum;
-        } else {
-            answer = common[(common.length - 1)] * mul;
-        }
-        
+
         return answer;
     }
 }
