68399f0bfdd366fba3769ba0c8336247fe532010
wooni <77907190+wooni89@users.noreply.github.com>
2023-12-06 12:07:48 +0900

    [Bronze V] Title: A+B, Time: 124 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,14 +1,18 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+public class Main{
+    public static void main(String[] args) throws IOException {
         
-        int a = in.nextInt();
-        int b = in.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str," ");
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        
         System.out.println(a+b);
-        
-        in.close();
     }
 }
-
